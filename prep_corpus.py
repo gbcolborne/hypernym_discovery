6a5f70d1310ec18974a3344cb3547b687c803358
gbcolborne <g.b.colborne@gmail.com>
2018-11-08 10:54:12 -0500

    Add prep_corpus.py and utils.py

---
+++ prep_corpus.py
@@ -0,0 +1,227 @@
+import argparse
+from collections import defaultdict
+import utils
+
+doc=""" Given a (pre-tokenized) corpus and a vocab of terms, convert
+words in corpus to lower case and replace multi-word terms in corpus
+with a single token (by replacing spaces with underscores), and
+optionally replace out-of-vocab tokens with <UNK>. Write resulting
+corpus and a TSV file containing the frequency of terms in vocab (path
+is like input corpus, but with suffix .vocab).
+
+"""
+
+def get_indices_unmasked_spans(mask):
+    """ Given a mask array (list where masked elements are evaluated
+    as True and unmasked elements are evaluated as False), return
+    spans of unmasked list items."""
+    spans = []
+    start = 0
+    while start < len(mask):
+        if mask[start]:
+            start += 1
+            continue
+        end = start
+        for i in range(start+1, len(mask)):
+            if mask[i]:
+                break
+            else:
+                end = i
+        spans.append((start, end))
+        start = end + 1
+    return spans
+
+def extract_ngrams(tokens, n, ngram_vocab, term_to_freq):
+    """ Given a list of tokens and a vocab of n-grams, extract list of
+    non-overlapping n-grams found in tokens, using term frequency to
+    resolve overlap, in a way that favours low-frequency n-grams.
+
+    Args:
+    - Tokens: list of tokens
+    - n: size of n-grams
+    - ngram_vocab: set of target n-grams
+    - term_to_freq: dict that maps terms to their frequency
+
+    Returns: 
+    - List of (index, term) tuples, where index is the index of the
+      first token of each n-gram, and term is the n-gram (joined with
+      spaces).
+
+    """
+    ngrams_found = []
+    for i in range(len(tokens)-n+1):
+        term = " ".join(tokens[i:i+n])
+        if term in ngram_vocab:
+            ngrams_found.append((i,term))
+    if len(ngrams_found) < 2:
+        return ngrams_found
+    # Eliminate overlap
+    ngrams_filtered = ngrams_found[:1]
+    for (start, term) in ngrams_found[1:]:
+        prev_start, prev_term = ngrams_filtered[-1]
+        if start - prev_start < n:
+            if term not in term_to_freq or term_to_freq[term] < term_to_freq[prev_term]:
+                ngrams_filtered[-1] = (start, term)
+        else:
+            ngrams_filtered.append((start, term))
+    return ngrams_filtered
+
+def get_formatted_sample(strings, max_sampled):
+    sub = strings[:max_sampled]
+    if len(strings) > max_sampled:
+        sub.append("... ({}) more".format(len(strings)-max_sampled))
+    return ", ".join(sub)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=doc)
+    parser.add_argument("subtask", choices=["1A", "1B", "1C", "2A", "2B"], help="subtask")
+    parser.add_argument("path_corpus", help="path of corpus (text file)")
+    msg = ("path of directory containing datasets for SemEval-2018 Task 9, "
+           "from which we extract vocab and queries")
+    parser.add_argument("dir_datasets", help=msg)
+    msg = """path of output file containing corpus (path of vocab will
+be created by adding the suffix .vocab)"""
+    parser.add_argument("path_output", help=msg)
+    parser.add_argument("-r", "--replace_OOV", action="store_true", help="Replace OOV tokens with <UNK>")
+    args = parser.parse_args()
+
+    # Load candidates and queries
+    print("Loading candidates and queries...")
+    candidates, queries = utils.load_vocab(args.dir_datasets, args.subtask, lower_queries=True) 
+    print("Nb candidates: {}".format(len(candidates)))
+    print("Nb queries: {}".format(len(queries)))
+    vocab = candidates.union(queries)
+    print("Size of vocab: {}".format(len(vocab)))
+    trigrams = set()
+    bigrams = set()
+    unigrams = set()
+    for term in vocab:
+        nb_words = len(term.split())
+        if nb_words == 3:
+            trigrams.add(term)
+        elif nb_words == 2:
+            bigrams.add(term)
+        elif nb_words == 1:
+            unigrams.add(term)
+        else:
+            msg = "Error: '{}' is not unigram, bigram or trigram".format(term)
+            raise ValueError(msg)
+    print("Nb unigrams: {}".format(len(unigrams)))
+    print("Nb bigrams: {}".format(len(bigrams)))
+    print("Nb trigrams: {}".format(len(trigrams)))
+    
+    # Count n-gram frequencies in corpus
+    print("Counting lines in corpus...")
+    nb_lines = sum(1 for line in open(args.path_corpus))
+    print("Counting n-gram frequencies in corpus...")
+    term_to_freq_in = defaultdict(int)
+    line_count = 0
+    with open(args.path_corpus) as f_in:
+        for line in f_in:
+            line_count += 1
+            if line_count % 100000 == 0:
+                msg = "{}/{} lines processed.".format(line_count, nb_lines)
+                msg += " Vocab coverage: {}/{}.".format(len(term_to_freq_in), len(vocab))
+                print(msg)
+            line = line.strip().replace("_", "")
+            words = [w.lower() for w in line.split()]
+            for n in [1,2,3]:
+                for i in range(len(words)+n-1):
+                    term = " ".join(words[i:i+n])
+                    if term in vocab:
+                        term_to_freq_in[term] += 1
+    msg = "{}/{} lines processed.".format(line_count, nb_lines)
+    msg += " Vocab coverage: {}/{}.".format(len(term_to_freq_in), len(vocab))
+    print(msg)
+    nb_missing_q = sum(1 for w in queries if term_to_freq_in[w] == 0)
+    nb_missing_c = sum(1 for w in candidates if term_to_freq_in[w] == 0)
+    print("Nb zero-frequency queries: {}".format(nb_missing_q))
+    print("Nb zero-frequency candidates: {}".format(nb_missing_c))
+
+    # Replace multi-word terms with single tokens
+    print("\nProcessing corpus...")
+    term_to_freq_out = defaultdict(int)
+    line_count = 0
+    with open(args.path_corpus) as f_in, open(args.path_output, "w") as f_out:
+        for line in f_in:
+            line_count += 1
+            if line_count % 100000 == 0:
+                msg = "{}/{} lines processed.".format(line_count, nb_lines)
+                msg += " Vocab coverage: {}/{}.".format(len(term_to_freq_out), len(vocab))
+                print(msg)
+            line = line.strip().replace("_", "")
+            words = [w.lower() for w in line.split()]
+            # Make array indicating the length of the term found at each
+            # position
+            term_lengths = [0 for _ in range(len(words))]
+            # Make array indicating which indices are masked because a
+            # term has already been found there
+            masked_indices = [0 for _ in range(len(words))]
+            # Check for trigrams
+            trigrams_found = extract_ngrams(words, 3, trigrams, term_to_freq_in)
+            for (i, term) in trigrams_found:
+                term_lengths[i] = 3
+                term_to_freq_out[term] += 1
+                masked_indices[i] = 1
+                masked_indices[i+1] = 1
+                masked_indices[i+2] = 1
+            # Check for bigrams
+            for (beg, end) in get_indices_unmasked_spans(masked_indices):    
+                bigrams_found = extract_ngrams(words[beg:end+1], 2, bigrams, term_to_freq_in)                
+                for (i, term) in bigrams_found:
+                    term_lengths[beg+i] = 2
+                    term_to_freq_out[term] += 1
+                    masked_indices[beg+i] = 1
+                    masked_indices[beg+i+1] = 1
+            # Check for unigrams
+            for (beg, end) in get_indices_unmasked_spans(masked_indices):    
+                for i in range(beg,end+1):
+                    term = words[i]
+                    if term in unigrams:
+                        term_to_freq_out[term] += 1
+                        term_lengths[i] = 1
+            # Write sentence
+            norm_terms = []
+            i = 0
+            while i < len(term_lengths):
+                n = term_lengths[i] 
+                if n > 1:
+                    norm_terms.append("_".join(words[i:i+n]))
+                    i += n
+                else:
+                    if args.replace_OOV and n == 0:
+                        norm_term = "<UNK>"
+                    else:
+                        norm_term = words[i]
+                    norm_terms.append(norm_term)
+                    i += 1
+            sent = " ".join(norm_terms)
+            f_out.write(sent+"\n")
+    msg = "{}/{} lines processed.".format(line_count, nb_lines)
+    msg += " Vocab coverage: {}/{}.".format(len(term_to_freq_out), len(vocab))
+    print(msg)
+    missing_q = [w for w in queries if term_to_freq_out[w] == 0]
+    missing_c = [w for w in candidates if term_to_freq_out[w] == 0]
+    print("Nb missing queries in output: {}".format(len(missing_q)))
+    max_shown = 200
+    if len(missing_q):
+        msg = "Examples: {}".format(get_formatted_sample(sorted(missing_q), max_shown))
+        print(msg)
+    print("Nb missing candidates in output: {}".format(len(missing_c)))
+    if len(missing_c):
+        msg = "Examples: {}".format(get_formatted_sample(sorted(missing_c), max_shown))
+        print(msg)
+
+    msg = "\nWrote corpus --> {}".format(args.path_output)
+    print(msg)
+
+    # Write frequencies
+    path_output = args.path_output + ".vocab"
+    with open(path_output, "w") as f:
+        for term, freq in sorted(term_to_freq_out.items(), key=lambda x:x[0]):
+            # Normalize term
+            term_norm = "_".join(term.split())
+            f.write("{}\t{}\n".format(term_norm, freq))
+    msg = "Wrote vocab --> {}".format(path_output)
+    print(msg)
---
+++ utils.py
@@ -0,0 +1,225 @@
+import codecs
+from copy import deepcopy
+import numpy as np
+import torch
+from torch import autograd
+from torch import nn
+
+def wrap_in_var(tensor, grad, cuda):
+    """ Wrap a TensorFlow tensor in a variable.
+
+    Args:
+    - tensor
+    - grad: do we need a gradient?
+    - cuda: are we using CUDA?
+
+    Returns:
+    - variable
+
+    """
+    if cuda:
+        return autograd.Variable(tensor.cuda(), requires_grad=grad)
+    else:
+        return autograd.Variable(tensor, requires_grad=grad)
+
+def make_embedder(embeds, grad=False, cuda=False, sparse=False):
+    """ Make an Embedding module from a numpy array.
+    Args:
+    - embeds: numpy array of embeddings, shape (nb embeddings, embedding size)
+
+    Keyword args:
+    - requires_grad: boolean which specifies whether grad is required 
+    - cuda: boolean which specifies whether we use cuda 
+    - sparse: boolean which specifies whether embeddings are sparse 
+    
+    """
+    nb_embeds, dim = embeds.shape
+    embed = nn.Embedding(nb_embeds, dim, sparse=sparse)
+    weights = deepcopy(embeds)
+    if cuda:
+        weight_data = torch.FloatTensor(weights).cuda()
+    else:
+        weight_data = torch.FloatTensor(weights)
+    embed.weight=nn.Parameter(weight_data)
+    embed.weight.requires_grad = grad
+    return embed
+
+def make_embedding_matrix(word2vec, words, seed=0):
+    """ Given a mapping of words to vectors and a list of words, make
+    a matrix containing the vector of each word. Assign a random
+    vector to words that don't have one.
+
+    Args:
+    - word2vec: dict that maps words to vectors
+    - words: list of words
+    - seed: seed for numpy's RNG
+
+    Returns:
+    - matrix: containing row vector for each word in same order as the
+      list of words
+
+    """
+    np.random.seed(seed)
+    dim = word2vec[words[0]].shape[0]
+    dtype = word2vec[words[0]].dtype
+    matrix = np.zeros((len(words), dim), dtype=dtype)
+    for (i,word) in enumerate(words):
+        if word in word2vec:
+            matrix[i] = word2vec[word]
+        else:
+            matrix[i] = np.random.uniform(low=-0.5, high=0.5) / dim
+    return matrix
+
+
+def normalize_numpy_matrix(x):
+    """ Make rows in a 2-D numpy array unit-length. """
+    return x / np.linalg.norm(x, ord=2, axis=1).reshape(-1,1)
+
+def get_embeddings(path, dtype=np.float32):
+    """Get word embeddings from text file. 
+
+    Args:
+    - path
+    - dtype: dtype of matrix
+
+    Returns:
+    - vocab (list of words) 
+    - dict that maps words to vectors
+
+    """
+    # Get vector size
+    with codecs.open(path, "r", "utf-8") as f:
+        elems = f.readline().strip().split()
+        if len(elems) == 2:
+            header = True
+            dim = int(elems[1])
+        else:
+            header = False
+            dim = len(elems)-1
+    words = []
+    word2vec = {}
+    with codecs.open(path, "r", "utf-8") as f:
+        line_count = 0
+        if header:
+            f.readline()
+            line_count = 1
+        for line in f:
+            line_count += 1
+            elems = line.strip().split()
+            if len(elems) == dim + 1:
+                word = elems[0]
+                try:
+                    vec = np.asarray(elems[1:], dtype=dtype)
+                    words.append(word)
+                    word2vec[word] = vec
+                except ValueError as e:
+                    print("ValueError: Skipping line {}".format(line_count))
+            else:
+                msg = "Error: Skipping line {}. ".format(line_count)
+                msg += "Expected {} elements, found {}.".format(dim+1, len(elems))
+                print()
+    return words, word2vec
+
+def expand_subtask_name(subtask):
+    """ Given short name of subtask, return long name. """
+    if subtask == "1A":
+        return "1A.english"
+    elif subtask == "1B":
+        return "1B.italian"
+    elif subtask == "1C":
+        return "1C.spanish"
+    elif subtask == "2A":
+        return "2A.medical"
+    elif subtask == "2B":
+        return "2B.music"
+    else:
+        msg = "Unrecognized subtask name '{}'".format(subtask)
+        raise ValueError(msg)
+
+def load_vocab(path_data, subtask, lower_queries=False):
+    """ Given the path of the directory containing the data for
+    SemEval 2018 task 9 and the name of a subtask (e.g. 1A), load
+    candidates and queries (training, trial, and test). Return set of
+    candidates and set of queries.
+    """
+    dataname = expand_subtask_name(subtask)
+    path = "{}/vocabulary/{}.vocabulary.txt".format(path_data, dataname)
+    candidates = set(load_candidates(path, normalize=False))
+    path = "{}/vocabulary/{}.vocabulary.txt".format(path_data, dataname)
+    queries = set()
+    for part in ["training", "trial", "test"]:
+        path = "{}/{}/data/{}.{}.data.txt".format(path_data, part, dataname, part)
+        q, _ = load_queries(path, normalize=False)
+        if lower_queries:
+            q = [query.lower() for query in q]
+        queries.update(q)
+    return candidates, queries
+
+def load_queries(path, normalize=True):
+    """Given the path of a query file, return list of queries and list
+    of query types.
+
+    """
+    with codecs.open(path, "r", encoding="utf-8") as f:
+        queries = []
+        query_types = []
+        for line in f:
+            q, qtype = line.strip().split("\t")
+            if normalize:
+                q = normalize_term(q)
+            queries.append(q)
+            query_types.append(qtype)
+    return queries, query_types
+
+def load_hypernyms(path, normalize=True):
+    """Given the path of a hypernyms file, return list of lists of
+    hypernyms.
+
+    """
+    with codecs.open(path, "r", encoding="utf-8") as f:
+        hypernyms = []
+        for line in f:
+            h_list = line.strip().split("\t")
+            if normalize:
+                h_list = [normalize_term(h) for h in h_list]
+            hypernyms.append(h_list)
+    return hypernyms
+
+def load_candidates(path, normalize=True):
+    """Given the path of a list of candidate hypernyms, return list of
+    candidates.
+
+    """
+    with codecs.open(path, "r", encoding="utf-8") as f:
+        candidates = []
+        for line in f:
+            c = line.strip()
+            if len(c):
+                if normalize:
+                    c = normalize_term(c)
+                candidates.append(c)
+        return candidates
+
+def denormalize_term(candidate):
+    """ Reverse normalization of candidate by replacing underscores
+    with spaces."""
+    return candidate.replace("_", " ")
+
+def normalize_term(term):
+    """ Normalize a query or candidate: lower-case (which only affects
+    queries, as candidates are all lower-case) and replace spaces with
+    underscores."""
+    return term.lower().replace(" ", "_")
+
+def print_params(model):
+    """ Print parameters of a PyTorch model. """
+    for name, param in model.named_parameters():
+        device = "CPU"
+        if type(param.data).__module__ == "torch.cuda":
+            device = "GPU"
+        grad = "no"
+        if param.requires_grad:
+            grad = "yes"
+        msg = "- {} (on {}, grad={}) ".format(name, device, grad)    
+        print(msg)
+
