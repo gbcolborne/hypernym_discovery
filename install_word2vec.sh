3231d6ad34391a8de024ccfd2bb7e660f6317b5d
gbcolborne <g.b.colborne@gmail.com>
2018-11-16 09:53:07 -0500

    Add script to install word2vec

---
+++ install_word2vec.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/google-code-archive-source/v2/code.google.com/word2vec/source-archive.zip
+unzip source-archive.zip
+rm source-archive.zip
+cd word2vec/trunk
+make
