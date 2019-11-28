56e828b17298ef3a597f0713deead79f8e7146f4
gbcolborne <g.b.colborne@gmail.com>
2019-11-28 19:17:05 +0000

    Add script to convert Tensorboard data to CSV

---
+++ tabulate_tensorboard_runs.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import numpy as np
+import pandas as pd
+
+from collections import defaultdict
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+"""
+Tabulate Tensorboard summaries and write as csv.
+Code adappted from https://stackoverflow.com/a/52095336
+"""
+
+def tabulate_events(dpath):
+    summary_iterators = [EventAccumulator(os.path.join(dpath, dname)).Reload() for dname in os.listdir(dpath)]
+    tags = summary_iterators[0].Tags()['scalars']
+    for it in summary_iterators:
+        assert it.Tags()['scalars'] == tags
+    out = defaultdict(list)
+    steps = []
+    for tag in tags:
+        steps = [e.step for e in summary_iterators[0].Scalars(tag)]
+        for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
+            assert len(set(e.step for e in events)) == 1
+            out[tag].append([e.value for e in events])
+    return out, steps
+        
+
+def to_csv(dpath):
+    dirs = os.listdir(dpath)
+            
+    d, steps = tabulate_events(dpath)
+    tags, values = zip(*d.items())
+    np_values = np.array(values)
+            
+    for index, tag in enumerate(tags):
+        df = pd.DataFrame(np_values[index], index=steps, columns=dirs)
+        df.to_csv(get_file_path(dpath, tag))
+                
+                
+def get_file_path(dpath, tag):
+    file_name = tag.replace("/", "_") + '.csv'
+    folder_path = os.path.join(dpath, 'csv')
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    return os.path.join(folder_path, file_name)
+                    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir_runs")
+    args = parser.parse_args()
+    to_csv(args.dir_runs)
