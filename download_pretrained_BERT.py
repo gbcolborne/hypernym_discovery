a738092533dd9764acf28c80952f1a5ebd1c3a10
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2019-12-04 12:24:42 -0500

    Two minor fixes

--- download_pretrained_BERT.py
+++ download_pretrained_BERT.py
@@ -1,11 +1,11 @@
-import arparse
+import argparse
 import torch
 import transformers
 
 """ Download and save a pretrained BERT model and tokenizer. """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("model_name", type=str, default="bert-base-cased")
+parser.add_argument("--model_name", "-m", type=str, default="bert-base-cased")
 parser.add_argument("cache_dir", type=str, help="Directory where downloaded model and tokenizer will be saved.")
 args = parser.parse_args()
 
