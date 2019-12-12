3153b8167760b8a5c7d0260595873c265815ab17
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2019-12-12 08:33:25 -0500

    specify that gold hypernyms are tab-separated

--- split_training_set.py
+++ split_training_set.py
@@ -4,8 +4,8 @@ import utils
 """ Split training set into a random train/dev split. """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("train_queries", help="Path of training queries, one per line")
-parser.add_argument("train_gold", help="Path of gold hypernyms for the training queries")
+parser.add_argument("train_queries", help="Path of text file containing training queries, one per line")
+parser.add_argument("train_gold", help="Path of TSV file containing gold hypernyms for the training queries, separated by tabs")
 parser.add_argument("--dev_size", "-d", type=int, default=50)
 parser.add_argument("dir_out", help="Path of output directory")
 args = parser.parse_args()
@@ -29,9 +29,9 @@ train_indices = indices[args.dev_size:]
 if not os.path.exists(args.dir_out):
     os.mkdir(args.dir_out)
 pq = os.path.join(args.dir_out, "train.queries.txt")
-pg = os.path.join(args.dir_out, "train.gold.txt")
+pg = os.path.join(args.dir_out, "train.gold.tsv")
 utils.write_queries_and_hypernyms(queries, gold, pq, pg, indices=train_indices)
 pq = os.path.join(args.dir_out, "dev.queries.txt")
-pg = os.path.join(args.dir_out, "dev.gold.txt")
+pg = os.path.join(args.dir_out, "dev.gold.tsv")
 utils.write_queries_and_hypernyms(queries, gold, pq, pg, indices=dev_indices)
 
