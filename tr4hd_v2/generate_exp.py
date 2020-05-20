21bb21aed92232a729dd36185594af2ffb36a826
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-05-19 20:20:55 -0400

    Adapts tests to new options

--- tr4hd_v2/generate_exp.py
+++ tr4hd_v2/generate_exp.py
@@ -18,8 +18,6 @@ base_cmd += " --max_steps 200000 --logging_steps 5000 --save_steps 5000 --save_t
 # Add flags
 #base_cmd += " --freeze_query_encoder"
 #base_cmd += " --freeze_cand_encoder"
-#base_cmd += " --normalize_encodings"
-base_cmd += " --subsample_positives"
 
 # Set prefix for output directories
 output_prefix = "Out1"
@@ -29,18 +27,22 @@ param_key_to_name = {"bs":"per_gpu_train_batch_size",
                      "lr":"learning_rate",
                      "dp":"dropout_prob",
                      "ng":"nb_neg_samples",
-                     "gn":"max_grad_norm"}
+                     "gn":"max_grad_norm",
+                     "ss":"pos_subsampling_factor",
+                     "nf":"normalization_factor"}
 
 # Set param values we want to test
-named_param_values = [("bs", ["16"]),
-                      ("lr", ["1e-5"]),
-                      ("dp", ["0.2"]),
-                      ("ng", ["10"]),
-                      ("gn", ["10"])]
+named_param_values = {"bs": ["16"],
+                      "lr": ["1e-5"],
+                      "dp": ["0.2"],
+                      "ng": ["10"],
+                      "gn": ["10"],
+                      "ss": ["0.5"],
+                      "nf": ["0.0", "0.5", "1.0"]}
 
 # Generate all combinations
 settings = [{}]
-for key, values in named_param_values:
+for key, values in named_param_values.items():
     tmp = []
     for setting in settings:
         for value in values:
@@ -50,9 +52,6 @@ for key, values in named_param_values:
             tmp.append(new_setting)
     settings = tmp[:]
 
-# Remove specific combinations (e.g. to avoid using too much memory)
-settings = [x for x in settings if not (x["ng"] == "32" and x["bs"] == "32")]
-                      
 # Take a random sample of settings
 MAX_TESTS = 32
 seed=91500
@@ -63,8 +62,8 @@ if len(settings) > MAX_TESTS:
     
 # Make custom command for each test
 for setting in settings:
-    model_dir = "_".join([output_prefix, "Model"] + ["%s=%s"%(k,v) for k,v in setting.items()])
-    eval_dir = "_".join([output_prefix, "Eval"] + ["%s=%s"%(k,v) for k,v in setting.items()])
+    model_dir = "_".join([output_prefix, "Model"] + ["%s=%s"%(k,v) for k,v in setting.items() if len(named_param_values[k]) > 1])
+    eval_dir = "_".join([output_prefix, "Eval"] + ["%s=%s"%(k,v) for k,v in setting.items() if len(named_param_values[k]) > 1])
     custom_cmd = base_cmd
     custom_cmd += " --model_dir %s" % model_dir
     custom_cmd += " --eval_dir %s" % eval_dir
