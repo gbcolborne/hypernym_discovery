Fixes computation of the diversity metric

tr4hd_v2/run_spon.py
@@ -260,14 +260,13 @@ def evaluate(opt, model, tokenizer, eval_data, cand_inputs):
         ap_scores.append(ap)
     results = {"MAP": np.mean(ap_scores)}
     
-    # Compute diversity of predictions across queries
+    # Compute diversity of top-k predictions across queries
     top_candidates_and_scores = get_top_k_candidates_and_scores(y_probs)
     all_preds = []
     for i in range(nb_queries):
         for j in range(RANKING_CUTOFF):
             cand, score = top_candidates_and_scores[i][j]
-            all_preds.add(cand)
-    assert len(all_preds) == nb_queries * RANKING_CUTOFF
+            all_preds.append(cand)
     results["diversity"] = len(set(all_preds)) / len(all_preds)
 
     return results