Fixes softmax function

tr4hd/Scorer.py
@@ -185,11 +185,15 @@ class Scorer(torch.nn.Module):
         """ Exponentiate and normalize scores (i.e. compute softmax) for a single query (numerically stable).
 
         Args:
-        - logits: 1-D tensor of logits for a single query or 2-D tensor where each row contains the logits of a query.
+        - logits: 1-D tensor of logits for a single query or 2-D tensor where each *row* contains the logits of a query.
 
         """
-        assert len(logits.size()) in [1, 2]
-        softmax = torch.exp(logits - torch.logsumexp(logits, 0))
+        nb_axes = len(logits.size())
+        assert nb_axes in [1, 2]
+        if nb_axes == 1:
+            softmax = torch.exp(logits - torch.logsumexp(logits, 0))
+        elif nb_axes == 2:
+            softmax = torch.exp(logits - torch.logsumexp(logits, 1))
         return softmax
 
 